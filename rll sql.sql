39a9d40271c13ecfc0a37e2f1cef5c46e33042b9
PRIYANKA M V <134724191+priyankamv279@users.noreply.github.com>
2024-01-23 16:13:10 +0530

    mysql

---
+++ rll sql.sql
@@ -0,0 +1,18 @@
+create database medicare;
+use medicare;
+show tables;
+select * from admin;
+select * from cart;
+select * from contact_us;
+select * from medicines;
+select * from user;
+
+
+
+
+
+truncate table user;
+truncate table medicines;
+truncate table cart;
+truncate table contact_us;
+truncate table admin;
